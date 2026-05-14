83e1bddacb76cf58a5903935cf2f3eed56644ae4
Deepak4053 <163020140+Deepak4053@users.noreply.github.com>
2026-05-14 14:19:28 +0530

    Update solution for sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -1,15 +1,10 @@
 import numpy as np
 def sample_var_std(x):
-    n = len(x)
-    div = 1/(n-1)
-    sm = 0
+    x = np.array(x)
     mean = np.mean(x)
-    for i in range(n):
-        temp = x[i] - mean
-        sm += temp*temp
-    var =div*sm
+    var = np.sum((x - mean)**2) / (len(x) - 1)
     std = np.sqrt(var)
-    return var,std  
+    return var, std  
   
 
 
