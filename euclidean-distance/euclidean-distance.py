dbd408473b51250894d9f70a3bab01be9513eefd
Deepak4053 <163020140+Deepak4053@users.noreply.github.com>
2026-05-22 16:58:44 +0530

    Update solution for euclidean-distance

--- euclidean-distance/euclidean-distance.py
+++ euclidean-distance/euclidean-distance.py
@@ -5,10 +5,9 @@ def euclidean_distance(x, y):
     Compute the Euclidean (L2) distance between vectors x and y.
     Must return a float.
     """
+    x =np.array(x)
+    y = np.array(y)
     if len(x)!= len(y):
         raise ValueError
-    dist = 0
-    for i in range(len(x)):
-        dist +=(x[i]-y[i])**2
-    dist =np.sqrt(dist)   
+    dist = np.sqrt(np.sum((x - y)**2))
     return dist
