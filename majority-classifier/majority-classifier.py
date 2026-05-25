d475d2509d2ad1c75f36d630a6280d4f94a3fd42
Deepak4053 <163020140+Deepak4053@users.noreply.github.com>
2026-05-25 15:17:46 +0530

    Update solution for majority-classifier

--- majority-classifier/majority-classifier.py
+++ majority-classifier/majority-classifier.py
@@ -1,15 +1,12 @@
-from collections import Counter
 import numpy as np
 
 def majority_classifier(y_train, X_test):
     """
-    Predict the most frequent label in training data for all test samples.
+    Predict the most frequent label in training data for all test samples using NumPy.
     """
-    freq = Counter(y_train)
-    most_common = freq.most_common()
-    majority_label = most_common[0][0]
-    ans = []
-    for i in range(len(X_test)):
-        ans.append(majority_label)
-        
+    labels, counts = np.unique(y_train, return_counts=True)
+    majority = np.argmax(counts)
+    majority_label = labels[majority]
+    
+    ans = np.full(shape=len(X_test), fill_value=majority_label)
     return ans
